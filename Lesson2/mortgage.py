c067fcf371affdee0983aa8deb1cb005486bc590
Eliza Boyce <elizaboyce@gmail.com>
2025-01-29 12:57:38 -0600

    Updated mortgage calculator

--- Lesson2/mortgage.py
+++ Lesson2/mortgage.py
@@ -5,58 +5,68 @@ with open('mortgage_prompts.json', 'r') as file:
 def prompt(message):
     print(f"==> {message}")
 
-prompt(MSG['welcome'])
-
 def validate(input_num):
     while True:
         try:
-            if float(input_num) >= 0:
-                return True
-            else:
-                return False
-        except Exception:
+            return float(input_num) >= 0
+        except ValueError:
             return False
 
-while True:
-    prompt(MSG['loan_amount'])
-    loan_amount = input()
-    if not validate(loan_amount):
-        prompt(MSG['invalid'])
+prompt(MSG['welcome'])
+keep_playing = "y"
+
+while not keep_playing.casefold() == "n":
+
+    while True:
+        prompt(MSG['loan_amount'])
+        loan_amount = input()
+        if not validate(loan_amount):
+            prompt(MSG['invalid'])
+        else:
+            break
+    loan_amount = float(loan_amount)
+
+    while True:
+        prompt(MSG['APR'])
+        annual_percentage_rate = input()
+        if not validate(annual_percentage_rate):
+            prompt(MSG['invalid'])
+        else:
+            break
+
+    if annual_percentage_rate == 0:
+        monthly_rate = 0
     else:
-        break
-loan_amount = float(loan_amount)
+        monthly_rate = (float(annual_percentage_rate) / 100) / 12
 
-while True:
-    prompt(MSG['APR'])
-    annual_percentage_rate = input()
-    if not validate(annual_percentage_rate):
-        prompt(MSG['invalid'])
+    while True:
+        prompt(MSG['loan_duration'])
+        loan_duration = input()
+        if not validate(loan_duration):
+            prompt(MSG['invalid'])
+        else:
+            break
+
+    loan_months = float(loan_duration)*12
+
+    if loan_amount == 0:
+        prompt(MSG["free_house"])
+    elif loan_months == 0:
+        prompt(MSG["zero_time"])
+    elif monthly_rate == 0:
+        prompt(MSG["principal_only"])
+        print(f"${loan_amount/loan_months:.2f}")
     else:
-        break
+        monthly_payment = loan_amount * (monthly_rate /
+            (1 - ((1 + monthly_rate) ** (-loan_months))))
+        prompt(MSG["std_payment"])
+        print(f"${monthly_payment:.2f}")
 
-if annual_percentage_rate == 0:
-    monthly_rate == 0
-else:
-    monthly_rate = (float(annual_percentage_rate) / 100) / 12
+    while True:
+        prompt(MSG["continue"])
+        keep_playing = input()
+        if keep_playing.casefold() in ["y", "n"]:
+            break
+        prompt(MSG["invalid"])
 
-while True:
-    prompt(MSG['loan_duration'])
-    loan_duration = input()
-    if not validate(loan_duration):
-        prompt(MSG['invalid'])
-    else:
-        break
-
-loan_months = float(loan_duration)*12
-
-if loan_amount == 0:
-    prompt(MSG["free_house"])
-elif loan_months == 0:
-    prompt(MSG["zero_time"])
-elif monthly_rate == 0:
-    prompt(MSG["principal_only"])
-    print(f"${loan_amount/loan_months:.2f}")
-else:
-    monthly_payment = loan_amount * (monthly_rate / (1 - ((1 + monthly_rate) ** (-loan_months))))
-    prompt(MSG["std_payment"])
-    print(f"${monthly_payment:.2f}")
+prompt(MSG["goodbye"])
